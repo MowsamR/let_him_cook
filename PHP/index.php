c098e9374b4fc92e904af2b193cc33f21f05eb11
Mowsam Rai <raimowsam@gmail.com>
2023-12-02 16:07:55 +0000

    Build connection

---
+++ PHP/index.php
@@ -0,0 +1,25 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    $servername = "localhost";
+    $username = "username";
+    $password = "password";
+
+    // Create connection
+    $conn = mysqli_connect($servername, $username, $password);
+    // Check connection
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+    echo "successfull connection";
+    ?>
+</body>
+
+</html>
